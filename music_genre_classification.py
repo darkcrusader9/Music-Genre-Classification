bd1bbd2b865d3d764670d885ea53b559544e2cbb
darkcrusader9 <50994964+darkcrusader9@users.noreply.github.com>
2019-10-18 05:19:01 +0530

    Add files via upload

---
+++ music_genre_classification.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 00:26:06 2019
+
+@author: Avas Ansuman
+"""
+
+import librosa
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+import os
+from PIL import Image
+import pathlib
+import csv
+
+# Preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+#Keras
+import keras
+
+import warnings
+warnings.filterwarnings('ignore')
+
+#
+cmap = plt.get_cmap('inferno')
+plt.figure(figsize=(10,10))
+genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
+for g in genres:
+    pathlib.Path(f'img_data/{g}').mkdir(parents=True, exist_ok=True)     
+    for filename in os.listdir(f'./MIR/genres/{g}'):
+        songname = f'./MIR/genres/{g}/{filename}'
+        y, sr = librosa.load(songname, mono=True, duration=5)
+        plt.specgram(y, NFFT=2048, Fs=2, Fc=0, noverlap=128, cmap=cmap, sides='default', mode='default', scale='dB');
+        plt.axis('off');
+        plt.savefig(f'img_data/{g}/{filename[:-3].replace(".", "")}.png')
+        plt.clf()
+#Extracting features from Spectrogram and writing to csv file
+header = 'filename chroma_stft rmse spectral_centroid spectral_bandwidth rolloff zero_crossing_rate'
+for i in range(1, 21):
+    header += f' mfcc{i}'
+header += ' label'
+header = header.split()
+file = open('data.csv', 'w', newline='')
+with file:
+    writer = csv.writer(file)
+    writer.writerow(header)
+genres = 'blues classical country disco hiphop jazz metal pop reggae rock'.split()
+for g in genres:
+    for filename in os.listdir(f'./MIR/genres/{g}'):
+        songname = f'./MIR/genres/{g}/{filename}'
+        y, sr = librosa.load(songname, mono=True, duration=30)
+        chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+        spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+        spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+        rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+        zcr = librosa.feature.zero_crossing_rate(y)
+        mfcc = librosa.feature.mfcc(y=y, sr=sr)
+        to_append = f'{filename} {np.mean(chroma_stft)} {np.mean(rmse)} {np.mean(spec_cent)} {np.mean(spec_bw)} {np.mean(rolloff)} {np.mean(zcr)}'    
+        for e in mfcc:
+            to_append += f' {np.mean(e)}'
+        to_append += f' {g}'
+        file = open('data.csv', 'a', newline='')
+        with file:
+            writer = csv.writer(file)
+            writer.writerow(to_append.split())
+            
+            
+#analysing the data
+data = pd.read_csv('data.csv')
+data.head()
+# Dropping unneccesary columns
+data = data.drop(['filename'],axis=1)
+
+genre_list = data.iloc[:, -1]
+encoder = LabelEncoder()
+y = encoder.fit_transform(genre_list)
+#scaling
+scaler = StandardScaler()
+X = scaler.fit_transform(np.array(data.iloc[:, :-1], dtype = float))
+#test set and training set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+#Building  Network
+from keras import models
+from keras import layers
+#shallow model
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_shape=(X_train.shape[1],)))
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+history = model.fit(X_train,
+                    y_train,
+                    epochs=20,
+                    batch_size=128)
+#evaluating our model
+test_loss, test_acc = model.evaluate(X_test,y_test)
+print('test_acc: ',test_acc)
+#deeper model
+model = models.Sequential()
+model.add(layers.Dense(512, activation='relu', input_shape=(X_train.shape[1],)))
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(partial_x_train,
+          partial_y_train,
+          epochs=30,
+          batch_size=512,
+          validation_data=(x_val, y_val))
+results = model.evaluate(X_test, y_test)
+test_loss, test_acc = model.evaluate(X_test,y_test)
+print('test_acc: ',test_acc)
